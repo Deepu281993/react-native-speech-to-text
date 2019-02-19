96c646899e714d8800e9d98a345fd9a01a8a1806
Deepu281993 <46395480+Deepu281993@users.noreply.github.com>
2019-02-19 16:21:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import { NativeModules } from 'react-native';
 
 let STTandroid =  NativeModules.SpeechToText;
 //let STTios =  NativeModules.SpeechTT;
-
-export { STTandroid, STTios };
+//export { STTandroid, STTios };
+export { STTandroid};
