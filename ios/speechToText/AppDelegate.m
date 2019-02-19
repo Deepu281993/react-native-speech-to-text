fcaa9743d7fd3ed75366f490c03739e3dac266ee
Deepu281993 <46395480+Deepu281993@users.noreply.github.com>
2019-02-19 17:03:15 +0530

    Update AppDelegate.m

--- ios/speechToText/AppDelegate.m
+++ ios/speechToText/AppDelegate.m
@@ -16,22 +16,22 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  NSURL *jsCodeLocation;
+//   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+//   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"speechToText"
-                                               initialProperties:nil
-                                                   launchOptions:launchOptions];
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+//   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+//                                                       moduleName:@"speechToText"
+//                                                initialProperties:nil
+//                                                    launchOptions:launchOptions];
+//   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
-  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  UIViewController *rootViewController = [UIViewController new];
-  rootViewController.view = rootView;
-  self.window.rootViewController = rootViewController;
-  [self.window makeKeyAndVisible];
-  return YES;
+//   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+//   UIViewController *rootViewController = [UIViewController new];
+//   rootViewController.view = rootView;
+//   self.window.rootViewController = rootViewController;
+//   [self.window makeKeyAndVisible];
+//   return YES;
 }
 
 @end
